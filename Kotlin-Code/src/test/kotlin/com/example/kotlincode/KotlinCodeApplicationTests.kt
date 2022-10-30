e2e0d18e858350de4f194175e13075c46c70c0b1
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2022-10-30 16:24:16 +0900

    ⚙️ :: 프로젝트 세팅

---
+++ Kotlin-Code/src/test/kotlin/com/example/kotlincode/KotlinCodeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotlincode
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinCodeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}

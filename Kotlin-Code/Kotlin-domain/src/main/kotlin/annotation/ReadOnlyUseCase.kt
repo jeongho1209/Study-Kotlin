69011b262931691d947214bdcf4b5089f00757dc
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2022-11-05 22:55:51 +0900

    ⚡️ :: 어노테이션

---
+++ Kotlin-Code/Kotlin-domain/src/main/kotlin/annotation/ReadOnlyUseCase.kt
@@ -0,0 +1,5 @@
+package annotation
+
+@Retention(AnnotationRetention.RUNTIME)
+@Target(AnnotationTarget.CLASS)
+annotation class ReadOnlyUseCase()
--- kotlin/build.gradle.kts
+++ kotlin/build.gradle.kts
@@ -1,11 +1,12 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-plugins {
-    id("org.springframework.boot") version "2.7.5"
-    id("io.spring.dependency-management") version "1.0.15.RELEASE"
-    kotlin("jvm") version "1.6.21"
-    kotlin("plugin.spring") version "1.6.21"
-}
+        plugins {
+            id("org.springframework.boot") version "2.6.6"
+            id("io.spring.dependency-management") version "1.0.11.RELEASE"
+            kotlin("jvm") version "1.6.10"
+            kotlin("plugin.spring") version "1.6.10"
+            kotlin("plugin.jpa") version "1.6.10"
+        }
 
 group = "com.example"
 version = "0.0.1-SNAPSHOT"
@@ -22,13 +23,34 @@ repositories {
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
+
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
+
+    runtimeOnly("mysql:mysql-connector-java")
+
+    implementation("org.springframework.boot:spring-boot-starter-security")
+
+    implementation("io.jsonwebtoken:jjwt:0.9.0")
+
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+allOpen {
+    annotation("javax.persistence.Entity")
+}
+
+tasks.getByName<Jar>("jar") {
+    enabled = false
 }
 
 tasks.withType<KotlinCompile> {

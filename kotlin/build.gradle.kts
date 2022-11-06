b18cb5771a755abd332fb226eb20f66038b762ec
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2022-11-06 19:26:42 +0900

    ⚡️ :: Validation 의존성 추가

--- kotlin/build.gradle.kts
+++ kotlin/build.gradle.kts
@@ -28,6 +28,8 @@ dependencies {
 
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 

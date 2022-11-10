c8aa31e6275a700bcd2419602c6e8db8a37c6d58
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2022-11-10 21:25:27 +0900

    📑 :: SecurityConfig

---
+++ kotlin/src/main/kotlin/com/example/kotlin/global/security/SecurityConfig.kt
@@ -0,0 +1,35 @@
+package com.example.kotlin.global.security
+
+import org.springframework.context.annotation.Bean
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+
+@EnableWebSecurity
+class SecurityConfig(
+
+) : WebSecurityConfigurerAdapter() {
+
+    override fun configure(http: HttpSecurity) {
+        http
+                .csrf().disable()
+                .formLogin().disable()
+                .cors()
+
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                .and()
+                .authorizeRequests()
+
+                .antMatchers("/**").permitAll()
+    }
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
+
+}

48ffebe435a46229e6ef574efe03a5068c2977b8
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2022-11-16 22:45:51 +0900

    ♻️ :: Security 로직 수정

--- kotlin/src/main/kotlin/com/example/kotlin/global/security/SecurityConfig.kt
+++ kotlin/src/main/kotlin/com/example/kotlin/global/security/SecurityConfig.kt
@@ -1,32 +1,40 @@
 package com.example.kotlin.global.security
 
+import com.example.kotlin.global.security.jwt.JwtTokenProvider
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.context.annotation.Bean
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
 
 @EnableWebSecurity
 class SecurityConfig(
+        private val jwtTokenProvider: JwtTokenProvider,
+        private val objectMapper: ObjectMapper
+) {
 
-) : WebSecurityConfigurerAdapter() {
-
-    override fun configure(http: HttpSecurity) {
+    @Throws(Exception::class)
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
         http
                 .csrf().disable()
                 .formLogin().disable()
-                .cors()
-
-                .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-
-                .and()
+                .and().cors()
+        http
                 .authorizeRequests()
 
                 .antMatchers("/**").permitAll()
+
+                .anyRequest().permitAll()
+
+                .and().apply(FilterConfig(jwtTokenProvider, objectMapper))
+
+        return http.build()
     }
 
     @Bean

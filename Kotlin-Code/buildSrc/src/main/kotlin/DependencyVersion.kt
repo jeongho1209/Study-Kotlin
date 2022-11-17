7185284f71331113dbaed6257a51acc9a6584b1b
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2022-11-18 08:13:17 +0900

    ⚙️ :: 의존성 세팅

---
+++ Kotlin-Code/buildSrc/src/main/kotlin/DependencyVersion.kt
@@ -0,0 +1,7 @@
+object DependencyVersion {
+    const val CLOUD_AWS = "2.2.6.RELEASE"
+    const val KTLINT_VERSION = "0.44.0"
+    const val QUERYDSL = "5.0.0"
+    const val OPENFEIGN = "3.1.4"
+    const val TRANSACTION = "5.3.23"
+}

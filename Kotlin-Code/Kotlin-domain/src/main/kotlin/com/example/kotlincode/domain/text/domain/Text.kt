78bcf35c0afe51ee4b6921888c330297eb2f988c
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2022-11-18 08:24:36 +0900

    ⚡️ :: 도메인들 추가

---
+++ Kotlin-Code/Kotlin-domain/src/main/kotlin/com/example/kotlincode/domain/text/domain/Text.kt
@@ -0,0 +1,13 @@
+package com.example.kotlincode.domain.text.domain
+
+import com.example.kotlincode.annotation.Aggregate
+
+@Aggregate
+data class Text(
+        val id: Long,
+
+        val text: String,
+
+        val user: Long
+
+)

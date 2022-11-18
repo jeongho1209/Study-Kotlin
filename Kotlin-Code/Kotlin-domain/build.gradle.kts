87864b4f44f6466a8063304cb1aa380b3dde73e1
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2022-11-18 22:50:27 +0900

    ⚡️ :: open 추가

--- Kotlin-Code/Kotlin-domain/build.gradle.kts
+++ Kotlin-Code/Kotlin-domain/build.gradle.kts
@@ -1,3 +1,12 @@
+plugins {
+    kotlin("plugin.allopen") version PluginVersions.ALLOPEN_VERSION
+}
+
 dependencies {
 
+}
+
+allOpen {
+    annotation("com.example.kotlincode.annotation.ReadOnlyUseCase")
+    annotation("com.example.kotlincode.annotation.UseCase")
 }

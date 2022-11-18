b29af3ae6e4873f6412f771bf3b24894ea357895
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2022-11-18 22:49:48 +0900

    ⚡️ :: 의존성 추가

--- Kotlin-Code/buildSrc/src/main/kotlin/PluginVersions.kt
+++ Kotlin-Code/buildSrc/src/main/kotlin/PluginVersions.kt
@@ -5,4 +5,5 @@ object PluginVersions {
     const val PLUGIN_SPRING_VERSION = "1.6.21"
     const val PLUGIN_JPA_VERSION = "1.6.21"
     const val KAPT_VERSION = "1.6.10"
+    const val ALLOPEN_VERSION = "1.6.21"
 }

4b825355cc3fa8522094ab559cd48b229865f671
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2022-11-18 15:52:32 +0900

    🔀 :: Text 엔티티 생성

---
+++ Kotlin-Code/Kotlin-infrastructure/src/main/kotlin/com/example/kotlincode/domain/text/mapper/TextMapper.kt
@@ -0,0 +1,32 @@
+package com.example.kotlincode.domain.text.mapper
+
+import com.example.kotlincode.domain.GenericMapper
+import com.example.kotlincode.domain.text.domain.Text
+import com.example.kotlincode.domain.text.persistence.TextEntity
+import com.example.kotlincode.domain.user.persistence.entity.UserRepository
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Component
+
+@Component
+class TextMapper(
+        private val userRepository: UserRepository
+) : GenericMapper<TextEntity, Text> {
+
+    override fun toDomain(entity: TextEntity): Text = entity.let {
+        Text(
+                id = it.id,
+                text = it.text,
+                user = it.userEntity.id
+        )
+    }
+
+    override fun toEntity(domain: Text): TextEntity {
+        val user = userRepository.findByIdOrNull(domain.user)!!
+        return TextEntity(
+                id = domain.id,
+                text = domain.text,
+                userEntity = user
+        )
+    }
+
+}

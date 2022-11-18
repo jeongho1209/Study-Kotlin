2ae17773cdbbdab761121915e4783f0c70cc1ef7
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2022-11-18 19:02:02 +0900

    ⚙️ :: 의존성 정리

--- Kotlin-Code/Kotlin-domain/src/main/kotlin/com/example/kotlincode/annotation/UseCase.kt
+++ Kotlin-Code/Kotlin-domain/src/main/kotlin/com/example/kotlincode/annotation/UseCase.kt
@@ -1,8 +1,5 @@
 package com.example.kotlincode.annotation
 
-import org.springframework.transaction.annotation.Transactional
-
 @Retention(AnnotationRetention.RUNTIME)
 @Target(AnnotationTarget.CLASS)
-@Transactional
 annotation class UseCase()

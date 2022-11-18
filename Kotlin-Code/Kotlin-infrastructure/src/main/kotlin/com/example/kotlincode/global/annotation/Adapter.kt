4ebd3d90329380ecd410b2f8111fbf668fffa99c
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2022-11-18 22:53:33 +0900

    ⚡️ :: @Adapter 추가

--- Kotlin-Code/Kotlin-infrastructure/src/main/kotlin/com/example/kotlincode/global/annotation/Adapter.kt
+++ Kotlin-Code/Kotlin-infrastructure/src/main/kotlin/com/example/kotlincode/global/annotation/Adapter.kt
@@ -5,4 +5,4 @@ import org.springframework.stereotype.Component
 @Retention(AnnotationRetention.RUNTIME)
 @Target(AnnotationTarget.CLASS)
 @Component
-annotation class Adapter()
+annotation class Adapter

53524f8b0bc11a8cb75701a6ee76de18b02d6864
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2022-11-18 22:52:51 +0900

    ♻️ :: 파일 구조 수정

--- Kotlin-Code/Kotlin-infrastructure/src/main/kotlin/com/example/kotlincode/domain/text/persistence/TextRepository.kt
+++ Kotlin-Code/Kotlin-infrastructure/src/main/kotlin/com/example/kotlincode/domain/text/persistence/TextRepository.kt
@@ -1,5 +1,6 @@
 package com.example.kotlincode.domain.text.persistence
 
+import com.example.kotlincode.domain.text.persistence.entity.TextEntity
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 

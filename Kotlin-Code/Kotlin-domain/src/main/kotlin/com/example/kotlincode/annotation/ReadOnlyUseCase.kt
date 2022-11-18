7a00151d6ef00843a1f9fffd09c78e01218701e6
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2022-11-18 13:28:07 +0900

    🔀 :: 의존성 추가

--- Kotlin-Code/Kotlin-domain/src/main/kotlin/com/example/kotlincode/annotation/ReadOnlyUseCase.kt
+++ Kotlin-Code/Kotlin-domain/src/main/kotlin/com/example/kotlincode/annotation/ReadOnlyUseCase.kt
@@ -1,5 +1,8 @@
 package com.example.kotlincode.annotation
 
+import org.springframework.transaction.annotation.Transactional
+
 @Retention(AnnotationRetention.RUNTIME)
 @Target(AnnotationTarget.CLASS)
+@Transactional(readOnly = true)
 annotation class ReadOnlyUseCase()
--- Kotlin-Code/Kotlin-domain/src/main/kotlin/com/example/kotlincode/annotation/UseCase.kt
+++ Kotlin-Code/Kotlin-domain/src/main/kotlin/com/example/kotlincode/annotation/UseCase.kt
@@ -1,5 +1,8 @@
 package com.example.kotlincode.annotation
 
+import org.springframework.transaction.annotation.Transactional
+
 @Retention(AnnotationRetention.RUNTIME)
 @Target(AnnotationTarget.CLASS)
+@Transactional
 annotation class UseCase()

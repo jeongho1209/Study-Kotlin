9b96e3e93528e422b088663a8d120d4c7617da02
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2022-11-18 15:53:33 +0900

    ⚡️ :: 게시글 수정 api

--- kotlin/src/main/kotlin/com/example/kotlin/domain/feed/presentation/FeedController.kt
+++ kotlin/src/main/kotlin/com/example/kotlin/domain/feed/presentation/FeedController.kt
@@ -1,8 +1,10 @@
 package com.example.kotlin.domain.feed.presentation
 
 import com.example.kotlin.domain.feed.presentation.dto.request.CreateFeedRequest
+import com.example.kotlin.domain.feed.presentation.dto.request.UpdateFeedRequest
 import com.example.kotlin.domain.feed.service.CreateFeedService
 import com.example.kotlin.domain.feed.service.DeleteFeedService
+import com.example.kotlin.domain.feed.service.UpdateFeedService
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 import javax.validation.Valid
@@ -12,6 +14,7 @@ import javax.validation.Valid
 class FeedController(
         private val createFeedService: CreateFeedService,
         private val deleteFeedService: DeleteFeedService,
+        private val updateFeedService: UpdateFeedService,
 ) {
 
     @ResponseStatus(HttpStatus.CREATED)
@@ -24,6 +27,13 @@ class FeedController(
     @DeleteMapping("/{feed-id}")
     fun deleteFeed(@PathVariable("feed-id") feedId: Long) {
         deleteFeedService.execute(feedId)
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("/{feed-id}")
+    fun updateFeed(@PathVariable("feed-id") feedId: Long,
+                   @RequestBody @Valid request: UpdateFeedRequest) {
+        updateFeedService.execute(feedId, request)
     }
 
 }

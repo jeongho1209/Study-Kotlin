530a106e13a03bc37ab789ded7888262b3416511
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2022-11-22 23:24:34 +0900

    ♻️ :: buildSrc 수정

---
+++ kotlin/buildSrc/src/main/kotlin/Dependency.kt
@@ -0,0 +1,40 @@
+object Dependency {
+
+    // validation
+    const val VALIDATION = "org.springframework.boot:spring-boot-starter-validation"
+
+    // web
+    const val WEB = "org.springframework.boot:spring-boot-starter-web"
+
+    // jackson
+    const val JACKSON = "com.fasterxml.jackson.module:jackson-module-kotlin"
+
+    // jpa
+    const val JPA = "org.springframework.boot:spring-boot-starter-data-jpa"
+
+    // reflect
+    const val REFLECT = "org.jetbrains.kotlin:kotlin-reflect"
+
+    // jdk8
+    const val STDLIB_JDK8 = "org.jetbrains.kotlin:kotlin-stdlib-jdk8"
+
+    // security
+    const val SPRING_SECURITY = "org.springframework.boot:spring-boot-starter-security"
+    const val JWT = "io.jsonwebtoken:jjwt:0.9.1"
+
+    // redis
+    const val REDIS = "org.springframework.boot:spring-boot-starter-data-redis"
+
+    // queryDSL
+    const val QUERYDSL = "com.querydsl:querydsl-jpa:${DependencyVersion.QUERYDSL}"
+    const val QUERYDSL_PROCESSOR = "com.querydsl:querydsl-apt:${DependencyVersion.QUERYDSL}:jpa"
+
+    // open feign
+    const val OPENFEIGN = "org.springframework.cloud:spring-cloud-starter-openfeign:${DependencyVersion.OPENFEIGN}"
+
+    // MySQL
+    const val MYSQL = "mysql:mysql-connector-java"
+
+    // property
+    const val PROPERTIES_SCAN = "org.springframework.boot:spring-boot-configuration-processor"
+}
---
+++ kotlin/buildSrc/src/main/kotlin/DependencyVersion.kt
@@ -0,0 +1,4 @@
+object DependencyVersion {
+    const val QUERYDSL = "5.0.0"
+    const val OPENFEIGN = "3.1.4"
+}

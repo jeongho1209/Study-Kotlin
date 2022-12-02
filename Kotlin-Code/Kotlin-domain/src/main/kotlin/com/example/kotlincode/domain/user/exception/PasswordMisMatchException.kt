116a41eeb573445d77d2268a45a1fe5a42e6cc16
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2022-12-02 17:58:50 +0900

    ♻️ :: Exception 형식 수정

--- Kotlin-Code/Kotlin-domain/src/main/kotlin/com/example/kotlincode/domain/user/exception/PasswordMisMatchException.kt
+++ Kotlin-Code/Kotlin-domain/src/main/kotlin/com/example/kotlincode/domain/user/exception/PasswordMisMatchException.kt
@@ -3,5 +3,5 @@ package com.example.kotlincode.domain.user.exception
 import com.example.kotlincode.domain.user.error.UserErrorCode
 import com.example.kotlincode.error.BusinessException
 
-object UserExistException : BusinessException(UserErrorCode.USER_EXIST) {
+object PasswordMisMatchException : BusinessException(UserErrorCode.PASSWORD_MIS_MATCH) {
 }

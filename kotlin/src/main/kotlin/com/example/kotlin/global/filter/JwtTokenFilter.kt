8e9b2c6c21b5d76cc3608a0ff9cc5223a5089b2f
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2022-12-03 16:41:50 +0900

    ♻️ :: 패키지 구조 수정

--- kotlin/src/main/kotlin/com/example/kotlin/global/filter/JwtTokenFilter.kt
+++ kotlin/src/main/kotlin/com/example/kotlin/global/filter/JwtTokenFilter.kt
@@ -1,9 +1,10 @@
 package com.example.kotlin.global.security.jwt
 
-import org.springframework.security.core.Authentication
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.filter.OncePerRequestFilter
+import java.io.IOException
 import javax.servlet.FilterChain
+import javax.servlet.ServletException
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
@@ -11,10 +12,14 @@ class JwtTokenFilter(
         private val jwtTokenProvider: JwtTokenProvider
 ) : OncePerRequestFilter() {
 
-    override fun doFilterInternal(request: HttpServletRequest, response: HttpServletResponse, filterChain: FilterChain) {
-        val bearer: String? = jwtTokenProvider.resolveToken(request)
-        if (bearer != null) {
-            val authentication: Authentication? = jwtTokenProvider.authentication(bearer)
+    override fun doFilterInternal(request: HttpServletRequest,
+                                  response: HttpServletResponse,
+                                  filterChain: FilterChain) {
+        val bearerToken = jwtTokenProvider.resolveToken(request)
+
+        bearerToken?.let {
+            val token = jwtTokenProvider.parseToken(bearerToken)
+            val authentication = jwtTokenProvider.authentication(token)
             SecurityContextHolder.getContext().authentication = authentication
         }
         filterChain.doFilter(request, response)

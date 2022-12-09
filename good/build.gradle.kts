57065ac5aea97ae8ad57f4c563f64bdd674cfc39
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2022-12-09 23:48:10 +0900

    ⚙️ :: buildSrc 적용

--- good/build.gradle.kts
+++ good/build.gradle.kts
@@ -1,55 +1,49 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "3.0.0"
-    id("io.spring.dependency-management") version "1.1.0"
-    kotlin("jvm") version "1.7.21"
-    kotlin("plugin.spring") version "1.7.21"
+    id("org.springframework.boot") version PluginVersions.SPRING_BOOT_FRAMEWORK_VERSION
+    id("io.spring.dependency-management") version PluginVersions.SPRING_DEPENDENCY_MANAGEMENT_VERSION
+    kotlin("jvm") version PluginVersions.JVM_VERSION
+    kotlin("plugin.spring") version PluginVersions.PLUGIN_SPRING_VERSION
+    kotlin("plugin.jpa") version PluginVersions.PLUGIN_JPA_VERSION
+    kotlin("kapt") version PluginVersions.KAPT_VERSION
 }
 
 group = "com.kotlin"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    implementation(Dependency.VALIDATION)
+    implementation(Dependency.WEB)
+    implementation(Dependency.JACKSON)
+    implementation(Dependency.JPA)
+    implementation(Dependency.REFLECT)
+    implementation(Dependency.STDLIB_JDK8)
+    implementation(Dependency.SPRING_SECURITY)
+    implementation(Dependency.REDIS)
+    kapt(Dependency.QUERYDSL_PROCESSOR)
+    implementation(Dependency.QUERYDSL)
+    runtimeOnly(Dependency.MYSQL)
+    implementation(Dependency.OPENFEIGN)
+    implementation(Dependency.JWT)
+    implementation(Dependency.PROPERTIES_SCAN)
+    implementation(Dependency.SENTRY)
+    implementation(Dependency.LOGBACK)
+}
 
-    // starter
-    implementation("org.springframework.boot:spring-boot-starter")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-
-    // jpa
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-
-    // web
-    implementation("org.springframework.boot:spring-boot-starter-web")
-
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-
-    // validation
-    implementation("org.springframework.boot:spring-boot-starter-validation")
-
-    // mysql
-    runtimeOnly("mysql:mysql-connector-java")
-
-    // security
-    implementation("org.springframework.boot:spring-boot-starter-security")
-
-    // jwt
-    implementation("io.jsonwebtoken:jjwt:0.9.1")
-
-    // configuration processor
-    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
-
-    // mockito
-    implementation("org.mockito.kotlin:mockito-kotlin")
-
+kotlin.sourceSets.main {
+    kotlin.srcDir("$buildDir/generated/source/kapt/main")
 }
 
 tasks.withType<KotlinCompile> {
@@ -61,9 +55,16 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+
 }
 
 allOpen {
+    annotation("javax.persistence.Entity")
+    annotation("javax.persistence.MappedSuperclass")
+    annotation("javax.persistence.Embeddable")
+}
+
+noArg {
     annotation("javax.persistence.Entity")
     annotation("javax.persistence.MappedSuperclass")
     annotation("javax.persistence.Embeddable")
---
+++ good/buildSrc/src/main/kotlin/DependencyVersion.kt
@@ -0,0 +1,6 @@
+object DependencyVersion {
+    const val QUERYDSL = "5.0.0"
+    const val OPENFEIGN = "3.1.4"
+    const val SENTRY = "6.8.0"
+    const val LOGBACK = "6.8.0"
+}

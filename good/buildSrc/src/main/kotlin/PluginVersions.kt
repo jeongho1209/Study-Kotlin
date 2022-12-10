c5be11ece5f89a3f79dc124da8619bd33f8f12a0
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2022-12-11 00:18:34 +0900

    ♻️ :: 버전 수정

--- good/buildSrc/src/main/kotlin/PluginVersions.kt
+++ good/buildSrc/src/main/kotlin/PluginVersions.kt
@@ -1,5 +1,5 @@
 object PluginVersions {
-    const val SPRING_BOOT_FRAMEWORK_VERSION = "3.0.0"
+    const val SPRING_BOOT_FRAMEWORK_VERSION = "2.7.2"
     const val SPRING_DEPENDENCY_MANAGEMENT_VERSION = "1.0.12.RELEASE"
     const val JVM_VERSION = "1.6.21"
     const val PLUGIN_SPRING_VERSION = "1.6.21"

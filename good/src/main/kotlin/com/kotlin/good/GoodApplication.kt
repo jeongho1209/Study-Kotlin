7ca559b31e6b880cfa56dec171b8cb7beb5b8e30
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2022-12-25 19:07:33 +0900

    📑 :: QuerydslConfig

--- good/src/main/kotlin/com/kotlin/good/GoodApplication.kt
+++ good/src/main/kotlin/com/kotlin/good/GoodApplication.kt
@@ -2,7 +2,9 @@ package com.kotlin.good
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
+@EnableJpaAuditing
 @SpringBootApplication
 class GoodApplication
 
---
+++ good/src/main/kotlin/com/kotlin/good/global/querydsl/QuerydslConfig.kt
@@ -0,0 +1,16 @@
+package com.kotlin.good.global.querydsl
+
+import com.querydsl.jpa.impl.JPAQueryFactory
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import javax.persistence.EntityManager
+
+@Configuration
+class QuerydslConfig(
+        private val entityManager: EntityManager
+) {
+
+    @Bean
+    fun jpaQueryFactory(): JPAQueryFactory = JPAQueryFactory(entityManager)
+
+}

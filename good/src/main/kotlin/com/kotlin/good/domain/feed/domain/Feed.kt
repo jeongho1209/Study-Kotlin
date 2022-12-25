426a9ac0b53ec644f9dc5b191e0a6ec77e6258aa
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2022-12-25 19:04:59 +0900

    ⚡️ :: @DynamicInsert 추가

--- good/src/main/kotlin/com/kotlin/good/domain/feed/domain/Feed.kt
+++ good/src/main/kotlin/com/kotlin/good/domain/feed/domain/Feed.kt
@@ -3,34 +3,36 @@ package com.kotlin.good.domain.feed.domain
 import com.kotlin.good.domain.user.domain.User
 import com.kotlin.good.global.entity.BaseUUIDEntity
 import org.hibernate.annotations.ColumnDefault
+import org.hibernate.annotations.DynamicInsert
 import org.springframework.data.annotation.CreatedDate
 import java.time.LocalDateTime
 import java.util.*
 import javax.persistence.*
 import javax.validation.constraints.NotNull
 
+@DynamicInsert
 @Entity
 @Table(name = "tbl_feed")
 class Feed(
-        override val id: UUID,
+    override val id: UUID,
 
-        @field:NotNull
-        @Column(columnDefinition = "VARCHAR(20)")
-        val title: String,
+    @field:NotNull
+    @Column(columnDefinition = "VARCHAR(20)")
+    val title: String,
 
-        @field:NotNull
-        @Column(columnDefinition = "VARCHAR(1000)")
-        val content: String,
+    @field:NotNull
+    @Column(columnDefinition = "VARCHAR(1000)")
+    val content: String,
 
-        @field:NotNull
-        @ColumnDefault("'0'")
-        val viewCounts: Int,
+    @field:NotNull
+    @ColumnDefault("'0'")
+    val viewCounts: Int,
 
-        @field:NotNull
-        @CreatedDate
-        val createdAt: LocalDateTime,
+    @field:NotNull
+    @CreatedDate
+    val createdAt: LocalDateTime,
 
-        @ManyToOne(fetch = FetchType.LAZY)
-        @JoinColumn(name = "user_id", nullable = false)
-        val user: User
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    val user: User
 ) : BaseUUIDEntity()

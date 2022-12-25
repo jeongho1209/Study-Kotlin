b5b3684d6349f589791a510fc7a5e99f55ab25b3
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2022-12-25 19:06:03 +0900

    ♻️ :: 가독성

--- good/src/main/kotlin/com/kotlin/good/domain/student/domain/Student.kt
+++ good/src/main/kotlin/com/kotlin/good/domain/student/domain/Student.kt
@@ -2,27 +2,27 @@ package com.kotlin.good.domain.student.domain
 
 import com.kotlin.good.domain.user.domain.User
 import com.kotlin.good.global.enum.Major
-import jakarta.persistence.*
-import jakarta.validation.constraints.NotNull
 import java.util.*
+import javax.persistence.*
+import javax.validation.constraints.NotNull
 
 @Entity
 @Table(name = "tbl_student")
 class Student(
-        @Id
-        val userId: UUID,
+    @Id
+    val userId: UUID,
 
-        @MapsId
-        @OneToOne(fetch = FetchType.LAZY, cascade = [CascadeType.ALL])
-        @JoinColumn(name = "user_id", columnDefinition = "BINARY(16)")
-        val user: User,
+    @MapsId
+    @OneToOne(fetch = FetchType.LAZY, cascade = [CascadeType.ALL])
+    @JoinColumn(name = "user_id", columnDefinition = "BINARY(16)")
+    val user: User,
 
-        @field:NotNull
-        @Column(columnDefinition = "VARCHAR(4)")
-        val gcn: String,
+    @field:NotNull
+    @Column(columnDefinition = "VARCHAR(4)")
+    val gcn: String,
 
-        @field:NotNull
-        @Enumerated(EnumType.STRING)
-        @Column(columnDefinition = "VARCHAR(8)")
-        val major: Major
+    @field:NotNull
+    @Enumerated(EnumType.STRING)
+    @Column(columnDefinition = "VARCHAR(8)")
+    val major: Major
 )
--- good/src/main/kotlin/com/kotlin/good/domain/user/domain/User.kt
+++ good/src/main/kotlin/com/kotlin/good/domain/user/domain/User.kt
@@ -3,37 +3,41 @@ package com.kotlin.good.domain.user.domain
 import com.kotlin.good.global.entity.BaseUUIDEntity
 import com.kotlin.good.global.enum.Authority
 import com.kotlin.good.global.enum.Sex
-import jakarta.persistence.*
-import jakarta.validation.constraints.NotNull
 import java.util.*
+import javax.persistence.*
+import javax.validation.constraints.NotNull
 
 @Entity
 @Table(name = "tbl_user")
 class User(
-        override val id: UUID,
+    override val id: UUID,
 
-        @field:NotNull
-        @Column(columnDefinition = "VARCHAR(64)", unique = true)
-        val email: String,
+    @field:NotNull
+    @Column(columnDefinition = "VARCHAR(64)", unique = true)
+    val email: String,
 
-        @field:NotNull
-        @Column(columnDefinition = "CHAR(60)")
-        val password: String,
+    @field:NotNull
+    @Column(columnDefinition = "CHAR(60)")
+    val password: String,
 
-        @field:NotNull
-        @Column(columnDefinition = "VARCHAR(10)")
-        val name: String,
+    @field:NotNull
+    @Column(columnDefinition = "VARCHAR(10)")
+    val name: String,
 
-        @field:NotNull
-        @Enumerated(EnumType.STRING)
-        @Column(columnDefinition = "VARCHAR(5)")
-        val authority: Authority,
+    @field:NotNull
+    @Column(columnDefinition = "VARCHAR(100)")
+    val address: String,
 
-        @field:NotNull
-        @Enumerated(EnumType.STRING)
-        @Column(columnDefinition = "VARCHAR(6)")
-        val sex: Sex,
+    @field:NotNull
+    @Enumerated(EnumType.STRING)
+    @Column(columnDefinition = "VARCHAR(5)")
+    val authority: Authority,
 
-        @Column(columnDefinition = "VARCHAR(50)")
-        val introduce: String?
+    @field:NotNull
+    @Enumerated(EnumType.STRING)
+    @Column(columnDefinition = "VARCHAR(6)")
+    val sex: Sex,
+
+    @Column(columnDefinition = "VARCHAR(50)")
+    val introduce: String?
 ) : BaseUUIDEntity()

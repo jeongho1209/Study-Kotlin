8dc586e9978b4c0be80680b9b44b6b3949f22d87
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2022-12-29 09:44:02 +0900

    ♻️ :: 토큰 파싱 메소드 수정

--- good/src/main/kotlin/com/kotlin/good/global/security/jwt/JwtTokenParser.kt
+++ good/src/main/kotlin/com/kotlin/good/global/security/jwt/JwtTokenParser.kt
@@ -5,7 +5,10 @@ import com.kotlin.good.global.exception.InternalServerError
 import com.kotlin.good.global.exception.InvalidToken
 import com.kotlin.good.global.security.SecurityProperty
 import com.kotlin.good.global.security.auth.AuthDetailsService
-import io.jsonwebtoken.*
+import io.jsonwebtoken.Claims
+import io.jsonwebtoken.ExpiredJwtException
+import io.jsonwebtoken.InvalidClaimException
+import io.jsonwebtoken.Jwts
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.Authentication
 import org.springframework.stereotype.Component
@@ -16,11 +19,11 @@ class JwtTokenParser(
     private val authDetailsService: AuthDetailsService
 ) {
 
-    private fun getClaims(token: String): Jws<Claims> {
+    private fun getClaims(token: String): Claims {
         return try {
             Jwts.parser()
                 .setSigningKey(securityProperty.secretKey)
-                .parseClaimsJws(token)
+                .parseClaimsJws(token).body
         } catch (e: Exception) {
             when (e) {
                 is InvalidClaimException -> throw InvalidToken.EXCEPTION
@@ -32,10 +35,10 @@ class JwtTokenParser(
 
     fun getAuthentication(token: String): Authentication {
         val claims = getClaims(token)
+        val email = claims.subject
+        val authDetails = authDetailsService.loadUserByUsername(email)
 
-        val details = authDetailsService.loadUserByUsername(claims.body.id)
-
-        return UsernamePasswordAuthenticationToken(details, "", details.authorities)
+        return UsernamePasswordAuthenticationToken(authDetails, "", authDetails.authorities)
     }
 
 }

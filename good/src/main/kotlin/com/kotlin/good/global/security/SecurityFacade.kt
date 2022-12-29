c1e0f3e325f6eba56688e3e379fd0c6fbf0e75c4
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2022-12-29 22:15:49 +0900

    ⚡️ :: getCurrentUser 메소드 추가

--- good/src/main/kotlin/com/kotlin/good/global/security/SecurityFacade.kt
+++ good/src/main/kotlin/com/kotlin/good/global/security/SecurityFacade.kt
@@ -1,14 +1,20 @@
 package com.kotlin.good.global.security
 
+import com.kotlin.good.domain.user.domain.User
+import com.kotlin.good.domain.user.domain.repository.UserRepository
+import com.kotlin.good.domain.user.error.exception.UserNotFound
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Component
-import java.util.*
 
 @Component
-class SecurityFacade {
+class SecurityFacade(
+    private val userRepository: UserRepository
+) {
 
-    fun getCurrentUserId(): UUID {
-        return UUID.fromString(SecurityContextHolder.getContext().authentication.name)
+    fun getCurrentUser(): User {
+        val email = SecurityContextHolder.getContext().authentication.name
+        return userRepository.findByEmail(email)
+            ?: throw UserNotFound.EXCEPTION
     }
 
 }

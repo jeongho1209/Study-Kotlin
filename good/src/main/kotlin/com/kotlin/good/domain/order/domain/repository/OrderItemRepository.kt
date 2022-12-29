444779c7fad04b7e0856bf3da54b9609c267f60a
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2022-12-29 22:14:14 +0900

    ⚡️ :: CustomRepository 상속

--- good/src/main/kotlin/com/kotlin/good/domain/order/domain/repository/OrderItemRepository.kt
+++ good/src/main/kotlin/com/kotlin/good/domain/order/domain/repository/OrderItemRepository.kt
@@ -1,10 +1,11 @@
 package com.kotlin.good.domain.order.domain.repository
 
 import com.kotlin.good.domain.order.domain.OrderItem
+import com.kotlin.good.domain.order.domain.repository.custom.CustomOrderItemRepository
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
-import java.util.UUID
+import java.util.*
 
 @Repository
-interface OrderItemRepository : CrudRepository<OrderItem, UUID> {
+interface OrderItemRepository : CrudRepository<OrderItem, UUID>, CustomOrderItemRepository {
 }
--- good/src/main/kotlin/com/kotlin/good/domain/order/domain/repository/OrderRepository.kt
+++ good/src/main/kotlin/com/kotlin/good/domain/order/domain/repository/OrderRepository.kt
@@ -1,6 +1,7 @@
 package com.kotlin.good.domain.order.domain.repository
 
 import com.kotlin.good.domain.order.domain.Order
+import com.kotlin.good.domain.order.domain.repository.custom.CustomOrderRepository
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 import java.util.*

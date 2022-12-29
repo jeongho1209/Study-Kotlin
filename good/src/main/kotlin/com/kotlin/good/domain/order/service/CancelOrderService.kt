cd30c91a79be975ffda4b9c670d3c1a96280b775
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2022-12-29 22:15:17 +0900

    ⚡️ :: 주문 취소 api

---
+++ good/src/main/kotlin/com/kotlin/good/domain/order/service/CancelOrderService.kt
@@ -0,0 +1,29 @@
+package com.kotlin.good.domain.order.service
+
+import com.kotlin.good.domain.order.error.exception.CannotCancelOrder
+import com.kotlin.good.domain.order.facade.OrderFacade
+import com.kotlin.good.global.security.SecurityFacade
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import java.util.*
+
+@Service
+class CancelOrderService(
+    private val securityFacade: SecurityFacade,
+    private val orderFacade: OrderFacade
+) {
+
+    @Transactional
+    fun execute(orderId: UUID) {
+        val order = orderFacade.getOrderById(orderId)
+
+        val user = securityFacade.getCurrentUser()
+
+        if (user != order.user) {
+            throw CannotCancelOrder.EXCEPTION
+        }
+
+        order.cancelOrder()
+    }
+
+}

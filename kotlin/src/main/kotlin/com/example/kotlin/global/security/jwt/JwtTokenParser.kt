56b792b894027b925a65a2b1b072fb5c0926e6e1
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2022-12-30 22:58:36 +0900

    ♻️ :: 토큰 파싱 코드 수정

--- kotlin/src/main/kotlin/com/example/kotlin/global/security/jwt/JwtTokenParser.kt
+++ kotlin/src/main/kotlin/com/example/kotlin/global/security/jwt/JwtTokenParser.kt
@@ -5,22 +5,25 @@ import com.example.kotlin.global.exception.InternalServerErrorException
 import com.example.kotlin.global.exception.InvalidTokenException
 import com.example.kotlin.global.security.SecurityProperties
 import com.example.kotlin.global.security.auth.AuthDetailsService
-import io.jsonwebtoken.*
+import io.jsonwebtoken.Claims
+import io.jsonwebtoken.ExpiredJwtException
+import io.jsonwebtoken.InvalidClaimException
+import io.jsonwebtoken.Jwts
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.Authentication
 import org.springframework.stereotype.Component
 
 @Component
 class JwtTokenParser(
-        private val securityProperties: SecurityProperties,
-        private val authDetailsService: AuthDetailsService
+    private val securityProperties: SecurityProperties,
+    private val authDetailsService: AuthDetailsService
 ) {
 
-    private fun getClaims(token: String): Jws<Claims> {
+    private fun getClaims(token: String): Claims {
         return try {
             Jwts.parser()
-                    .setSigningKey(securityProperties.secretKey)
-                    .parseClaimsJws(token)
+                .setSigningKey(securityProperties.secretKey)
+                .parseClaimsJws(token).body
         } catch (e: Exception) {
             when (e) {
                 is InvalidClaimException -> throw InvalidTokenException.EXCEPTION
@@ -32,9 +35,10 @@ class JwtTokenParser(
 
     fun getAuthentication(token: String): Authentication {
         val claims = getClaims(token)
+        val id = claims.subject
+        val authDetails = authDetailsService.loadUserByUsername(id.toString())
 
-        val details = authDetailsService.loadUserByUsername(claims.body.id)
-
-        return UsernamePasswordAuthenticationToken(details, "", details.authorities)
+        return UsernamePasswordAuthenticationToken(authDetails, "", authDetails.authorities)
     }
+
 }

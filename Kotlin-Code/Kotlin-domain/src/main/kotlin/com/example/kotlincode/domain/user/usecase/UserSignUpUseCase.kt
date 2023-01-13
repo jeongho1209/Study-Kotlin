15b6f9a0a962b9ded1078127ee5cab11641148fc
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-01-13 10:44:43 +0900

    ♻️ :: inbound port 삭제

--- Kotlin-Code/Kotlin-domain/src/main/kotlin/com/example/kotlincode/domain/user/usecase/UserSignUpUseCase.kt
+++ Kotlin-Code/Kotlin-domain/src/main/kotlin/com/example/kotlincode/domain/user/usecase/UserSignUpUseCase.kt
@@ -1,9 +1,8 @@
 package com.example.kotlincode.domain.user.usecase
 
 import com.example.kotlincode.annotation.UseCase
-import com.example.kotlincode.domain.user.api.UserSignUpApi
-import com.example.kotlincode.domain.user.api.dto.DomainUserSignUpRequest
 import com.example.kotlincode.domain.user.domain.User
+import com.example.kotlincode.domain.user.dto.DomainUserSignUpRequest
 import com.example.kotlincode.domain.user.exception.UserExistException
 import com.example.kotlincode.domain.user.spi.CommandUserSpi
 import com.example.kotlincode.domain.user.spi.QueryUserSpi
@@ -14,19 +13,19 @@ class UserSignUpUseCase(
         private val commandUserSpi: CommandUserSpi,
         private val queryUserSpi: QueryUserSpi,
         private val userSecuritySpi: UserSecuritySpi
-) : UserSignUpApi {
+) {
 
-    override fun execute(request: DomainUserSignUpRequest) {
+    fun execute(request: DomainUserSignUpRequest) {
         if (queryUserSpi.existsUserByAccountId(request.accountId)) {
-            throw UserExistException.EXCEPTION
+            throw UserExistException
         }
 
-        commandUserSpi.save(User(
-                id = 0,
-                accountId = request.accountId,
-                password = userSecuritySpi.encodePassword(request.password),
-                username = request.username
-        ))
+        commandUserSpi.save(
+                User(
+                        accountId = request.accountId,
+                        password = userSecuritySpi.encodePassword(request.password),
+                        username = request.username
+                ))
     }
 
 }

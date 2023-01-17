729101ce047b1f5c9ce575bf5d4d6edf811a9577
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-01-18 00:02:19 +0900

    ♻️ :: 스코프함수 let -> run으로 수정

--- Kotlin-Code/Kotlin-infrastructure/src/main/kotlin/com/example/kotlincode/domain/feed/mapper/FeedMapper.kt
+++ Kotlin-Code/Kotlin-infrastructure/src/main/kotlin/com/example/kotlincode/domain/feed/mapper/FeedMapper.kt
@@ -3,36 +3,38 @@ package com.example.kotlincode.domain.feed.mapper
 import com.example.kotlincode.domain.GenericMapper
 import com.example.kotlincode.domain.feed.domain.Feed
 import com.example.kotlincode.domain.feed.persistence.entity.FeedEntity
+import com.example.kotlincode.domain.user.exception.UserNotFoundException
 import com.example.kotlincode.domain.user.persistence.UserRepository
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Component
 
 @Component
 class FeedMapper(
-        private val userRepository: UserRepository
+    private val userRepository: UserRepository
 ) : GenericMapper<FeedEntity, Feed> {
 
-    override fun toDomain(entity: FeedEntity): Feed = entity.let {
+    override fun toDomain(entity: FeedEntity): Feed = entity.run {
         Feed(
-                id = it.id,
-                title = it.title,
-                content = it.content,
-                isCompleted = it.isCompleted,
-                createdAt = it.createdAt,
-                userId = it.userEntity.id
+            id = id,
+            title = title,
+            content = content,
+            isCompleted = isCompleted,
+            createdAt = createdAt,
+            userId = userEntity.id
         )
     }
 
     override fun toEntity(domain: Feed): FeedEntity {
-        val userEntity = userRepository.findByIdOrNull(domain.userId)!!
+        val userEntity = userRepository.findByIdOrNull(domain.userId)
+            ?: throw UserNotFoundException
 
         return FeedEntity(
-                id = domain.id,
-                title = domain.title,
-                content = domain.content,
-                createdAt = domain.createdAt,
-                isCompleted = domain.isCompleted,
-                userEntity = userEntity
+            id = domain.id,
+            title = domain.title,
+            content = domain.content,
+            createdAt = domain.createdAt,
+            isCompleted = domain.isCompleted,
+            userEntity = userEntity
         )
     }
 

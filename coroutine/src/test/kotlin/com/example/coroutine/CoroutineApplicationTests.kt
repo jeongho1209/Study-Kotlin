b5ad99118fc96103498033186f3719abe1302170
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-02-09 23:31:59 +0900

    ⚙️ :: setting

---
+++ coroutine/src/test/kotlin/com/example/coroutine/CoroutineApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.coroutine
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CoroutineApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}

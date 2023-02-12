6bf51bb96e251a645f47a37055d82e91af59335b
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-02-13 08:25:21 +0900

    ♻️ :: constructor 추가

--- kotlin/src/main/kotlin/com/example/kotlin/domain/feed/presentation/dto/response/FeedElement.kt
+++ kotlin/src/main/kotlin/com/example/kotlin/domain/feed/presentation/dto/response/FeedElement.kt
@@ -1,10 +1,18 @@
 package com.example.kotlin.domain.feed.presentation.dto.response
 
+import com.example.kotlin.domain.feed.domain.Feed
 import java.time.LocalDateTime
 
 data class FeedElement(
-        val feedId: Long,
-        val title: String,
-        val content: String,
-        val createdAt: LocalDateTime
-)
+    val feedId: Long,
+    val title: String,
+    val content: String,
+    val createdAt: LocalDateTime,
+) {
+    constructor(feed: Feed) : this(
+        feedId = feed.id,
+        title = feed.title,
+        content = feed.content,
+        createdAt = feed.createdAt,
+    )
+}

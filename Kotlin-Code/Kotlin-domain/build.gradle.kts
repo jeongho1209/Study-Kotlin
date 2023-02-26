4491a72159ce157b3b2254fe98d06fc4f972a572
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-02-27 00:28:42 +0900

    ♻️ :: 안 쓰는 의존성 정리

--- Kotlin-Code/Kotlin-domain/build.gradle.kts
+++ Kotlin-Code/Kotlin-domain/build.gradle.kts
@@ -1,12 +1,2 @@
 plugins {
-    kotlin("plugin.allopen") version PluginVersions.ALLOPEN_VERSION
-}
-
-dependencies {
-
-}
-
-allOpen {
-    annotation("com.example.kotlincode.annotation.ReadOnlyUseCase")
-    annotation("com.example.kotlincode.annotation.UseCase")
 }

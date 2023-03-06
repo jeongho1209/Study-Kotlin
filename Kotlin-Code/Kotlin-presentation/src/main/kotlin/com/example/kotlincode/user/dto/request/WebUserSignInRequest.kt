d8cb28a880744c76c50be549d3bd1ec95325198c
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-03-07 08:33:01 +0900

    ⚡️ :: TODO 추가

--- Kotlin-Code/Kotlin-presentation/src/main/kotlin/com/example/kotlincode/user/dto/request/WebUserSignInRequest.kt
+++ Kotlin-Code/Kotlin-presentation/src/main/kotlin/com/example/kotlincode/user/dto/request/WebUserSignInRequest.kt
@@ -1,4 +1,4 @@
-package com.example.kotlincode.domain.user.presentation.dto.request
+package com.example.kotlincode.user.dto.request
 
 import javax.validation.constraints.NotBlank
 
@@ -7,5 +7,5 @@ data class WebUserSignInRequest(
         val accountId: String,
 
         @NotBlank
-        val password: String
+        val password: String // TODO: Web 제거하기
 )

13918fa30d8d0d0cbd7bd5dd6c8ac6be6801104b
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-03-27 10:39:52 +0900

    ♻️ :: import 정리 && 코드 간결화

--- kotlin/src/main/kotlin/com/example/kotlin/domain/feed/service/CreateFeedService.kt
+++ kotlin/src/main/kotlin/com/example/kotlin/domain/feed/service/CreateFeedService.kt
@@ -5,27 +5,23 @@ import com.example.kotlin.domain.feed.domain.repository.FeedRepository
 import com.example.kotlin.domain.feed.presentation.dto.request.CreateFeedRequest
 import com.example.kotlin.domain.user.facade.UserFacade
 import org.springframework.stereotype.Service
-import org.springframework.transaction.annotation.Transactional
 import java.time.LocalDateTime
 
 @Service
 class CreateFeedService(
-        private val feedRepository: FeedRepository,
-        private val userFacade: UserFacade
+    private val feedRepository: FeedRepository,
+    private val userFacade: UserFacade
 ) {
 
-    @Transactional
     fun execute(request: CreateFeedRequest) {
-        val user = userFacade.getCurrentUser()
-
         feedRepository.save(
-                Feed(
-                        id = 0,
-                        title = request.title,
-                        content = request.content,
-                        user = user,
-                        createdAt = LocalDateTime.now()
-                ));
+            Feed(
+                id = 0,
+                title = request.title,
+                content = request.content,
+                user = userFacade.getCurrentUser(),
+                createdAt = LocalDateTime.now()
+            )
+        )
     }
-
 }
